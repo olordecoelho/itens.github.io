a4f013400774d38d245bf428f0dd06050edccc32
olordecoelho <olordecoelho@gmail.com>
2024-08-05 06:11:53 -0300

    que os jogos comecem

---
+++ src/app/armas.js
@@ -0,0 +1,71 @@
+export const ArmasDeCorte = [
+    {id:"ac001" , nome:"Adaga", dano:10, dec:"Parceira fiel e sorrateira de um usuário também sorrateiro." },
+    {id:"ac002" , nome:"Foice", dano:15 , dec:"É usada em colheitas, mas també serve para outros propósitos." },
+    {id:"ac003" , nome:"Machado", dano:25 , dec:"Anões baixinhos e carvalhos enormes caem perante essa arma.... Não irônicamente." },
+    {id:"ac004" , nome:"Espada curta", dano:30 , dec:"A fiel ecudeira de um nobre guerreiro, ataque e defesa em uma só empunhadura." },
+    {id:"ac005" , nome:"Rapiera", dano:20 , dec:"Veloz e mortal, uma lâmina fina e certiera. Ignora armadura leve. Viola armadura me{ida em 50%." },
+    {id:"ac006" , nome:"Claymore", dano:70 , dec:"É bem mais afiada que uma lâmina normal, e sim o tamanho importa." },
+    {id:"ac007" , nome:"Espada bastarda", dano:55 , dec:"Nobre arma dos cavalheiros reais." },
+    {id:"ac008" , nome:"Machado de guerra", dano:80 , dec:"Uma arma grande e assustadora, pra varrer campos de batalha. Causa dano em área." },
+    {id:"ac009" , nome:"Espadão", dano:100 , dec:"Você já sabe.... O tamanho importa." },
+    {id:"ac010" , nome:"Tamahawk", dano:40 , dec:"Um machado pequeno não parece tão assustador..... Ater ver ele vindo em sua direção." },
+    {id:"ac011" , nome:"Kreigmesser", dano:65 , dec:"Boa para ataque e defesa, pena que só corta de um lado." },
+    {id:"ac012" , nome:"Alabarda", dano:110 , dec:"Não apenas dilascera a carne e perfura armaduras, também mantem o inimigo longe. Ignora armadura média." },
+    {id:"ac013" , nome:"Machete", dano:45 , dec:"Nem espada nem adaga, estando bem afiada já é o bastante." },
+    {id:"ac014" , nome:"Cimitarra", dano:75 , dec:"Uma arma tipica de soldados das terras do sul." },
+    {id:"ac015" , nome:"Garras de aço", dano:50 , dec:"Inspirada nas feras, provem bom ataque e defesa." },
+    {id:"ac016" , nome:"Katana", dano:60 , dec:"Uma arma simples de uma terra distante, eficaz." },
+    {id:"ac017" , nome:"Kriss", dano:35 , dec:"Laminas pequenas e recurvas, não parece, mas causam muito dano. Triplo de dano se usado em sigilo." }
+];
+    
+export const ArmasDeImpacto = [
+    {id:"ai001" , nome:"Pau", dano:5, dec:"Era parte de uma árvore, com sorte você consegue se defender com ele." },
+    {id:"ai002" , nome:"Martelo", dano:15, dec:"Uma das razões para não irritar um carpinteiro." },
+    {id:"ai003" , nome:"Varal", dano:15, dec:"Um pau fino e compr{ido, mantem o oponente longe." },
+    {id:"ai004" , nome:"Tacape", dano:15, dec:"Um pau grosso e curto, vai doer mais que um normal." },
+    {id:"ai005" , nome:"Bastão de aço", dano:60, dec:"É precisa muita força para empunhar, mas cada golpe é tão brutal quanto o de uma espada." },
+    {id:"ai006" , nome:"Martelo de guerra", dano:80, dec:"Parece assustador, é assustador e é altamente mortal. Ignora completamente armaduras não magicas." },
+    {id:"ai007" , nome:"Manoplas", dano:20, dec:"Dependem um pouco das habil{idades de luta do usuário, mas são sim efetivas. (Nota do mestre: Pelo bem da história, não saia colocando pedras magicas nelas.)" },
+    {id:"ai008" , nome:"Quebra crânios", dano:100, dec:"O que será que aconece se fizer-mos um martelo do tamano de seu usuário? Bem, veja aí a resposta." },
+    {id:"ai009" , nome:"Chicote", dano:10, dec:"Não parece muito, mas seus golpes incapacitam a maioria dos ating{idos. Danos consecutivos incapacitam o oponente." },
+    {id:"ai010" , nome:"Munchako", dano:15, dec:"Requer muita habi{idade, mas com a pratica se torna letal. (Nota do mestre: Por alguma razão é uma arma popular entre abissais queloniformes.)" },
+    {id:"ai011" , nome:"Soco inglês", dano:5, dec:"Outra arma que depende da força e habil{idade do usuário, mas é boa para descontar a raiva." },
+    {id:"ai012" , nome:"Maça", dano:70, dec:"Uma arma pesada com um golpe igualmente pesado." },
+    {id:"ai103" , nome:"Bolas de aço", dano:50, dec:"Duas bolas de aço presas por uma corrente.... Sem mais descrições." }
+];
+    
+export const ArmasDeDisparo = [
+    {id:"ad001" , nome:"Arco", dano:10, dec:"Sem as flechas não é grande coisa, mas a reciproca é verdadeira" },
+    {id:"ad002" , nome:"Balestra", dano:20, dec:"Mais forte que um arco, só que muito mais lenta." },
+    {id:"ad003" , nome:"Shuriuken", dano:5, dec:"É preciso muita pontaria ou sorte para causarem danos significativos, mas são rap{idas e numerosas." },
+    {id:"ad004" , nome:"Funda" , dano:5 , dec:"Um couro cort{ido especialmente para lançar pedras.... Certo rei em uma terra MUITO distante matou um gigante com uma há varias eras." },
+    {id:"ad005" , nome:"Arco composto" , dano:50 , dec:"Requer mais força que um arco normal para ser usado, só que as flechas também precisam ser fortes." },
+    {id:"ad006" , nome:"Lança" , dano:30 , dec:"Pode sim ser arremessada, mas sinceramente não costuma ser uma boa {ideia, a não ser que...." },
+    {id:"ad007" , nome:"Pau de fogo" , dano:100 , dec:"Uma incrível criação dos anões, é praticamente um canhão em miniatura, mas qualquer um pode usar." },
+    {id:"ad008" , nome:"Lança chamas" , dano:100 , dec:"'O que é issso? AHHHHHHHHHHH.....' Foram as ultimas palavras dos infortunos inimigos das tropas de Lubrasia. Causa dano crítico se o alvo não possuir afin{idade elemental." },
+    {id:"ad009" , nome:"Arpão" , dano:40 , dec:"Algum pescador deve ter perd{ido isso, mas serve pra outros propósitos." },
+    {id:"ad0010" , nome:"Mini bombas" , dano:20 , dec:"Com a destreza correta derrubam qualquer inimigo, mas é melhor não esquecer onde as pôs." },
+    {id:"ad0011" , nome:"Bumerangue" , dano:20 , dec:"A arma mais fiel a seu usuário, se não voltar para sua mão é por que vocé é um péssimo usuário." },
+    {id:"ad0012" , nome:"Bumerangue de aço" , dano:50 , dec:"Fiel como o de madeira, mas recomendo usar uma proteção nas mãos, e evitar que chegue perto de seus pontos vitais enquanto voa." },
+    {id:"ad0013" , nome:"Dardos" , dano:3 , dec:"No melhor dos cenários furam os olhos do oponente, mes se você entender de venenos...." }
+];
+    
+export const ArmasMagicas = [
+    {id:"am001" , nome:"Anel piromante" , dano:20 , dec:"O fogo de seu anel poderá atormentar seus inimigos ou te proteger do frio." },
+    {id:"am002" , nome:"Anel h{idromante" , dano:20 , dec:"Muito útil para atrasar oponentes , ou enfrentar desertos escaldantes" },
+    {id:"am003" , nome:"Anel elétromante" , dano:20 , dec:"Não chega a ter o poder de uma tempestade, mas ainda pode ser letal" },
+    {id:"am004" , nome:"Anel geomante" , dano:20 , dec:"Move pedações de terra cons{ideráveis, nada perto de um deslizamento, mas é muito útil" },
+    {id:"am005" , nome:"Anel aeromante" , dano:20 , dec:"Pode te dar uma briza suave ou um vento cortante" },
+    {id:"am006" , nome:"Anel plantomante" , dano:20 , dec:"manipula plantas ao seu redor, por alguma razão herboristas fora da lei vendem até a alma por um desses." },
+    {id:"am007" , nome:"Anel criomante" , dano:20 , dec:"Gelo tem muitas util{idades, dentre todas as opções a que mais requer criativ{idade" },
+    {id:"am008" , nome:"Anel magnetomante" , dano:20 , dec:"Manipula metais de formas muito variadas, exelente contra inimigos armados" },
+    {id:"am009" , nome:"Anel hiperomante" , dano:5 , dec:"Muito mais tático do que ofensivo, mas é inegável que pode tirar a visão de um oponente." },
+    {id:"am0010" , nome:"Anel umbramante" , dano:5 , dec:"Muito mais tático do que ofensivo, mas é inegável que pode tirar a visão de um oponente." },
+    {id:"am0011" , nome:"Cajado condutor" , dano:50 , dec:"Depende de sua afin{idade elemental, mas em resumo condensa sua mana em um ataque com a essencia da mesma" },
+    {id:"am0012" , nome:"Orbe de contenção" , dano:100 , dec:"Uma armadilha bem eficaz para quem não a entende, mas lembre-se de não deixar o efeito passar. alvo incapacitado por 1 rodada." }
+];
+    
+export const ItensMagicos = [
+    {id:"im001" , nome:"Runa de mana" , amt:100 , dec:"Aumnta sua mana em 100, coisa de iniciante." },
+    {id:"im002" , nome:"orbe de mana" , amt:1000 , dec:"Concede 1000 de mana ao usuário, muito útil em momentos de necess{idade." }
+];
